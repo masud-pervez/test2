412c4a7d0da7deaaaeebde8a34294d512c31bea5
masudpervez05 <rocklucifer113@gmail.com>
2022-04-21 17:05:15 +0600

    01

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+})(jQuery);
