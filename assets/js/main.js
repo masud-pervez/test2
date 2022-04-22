fd159012517d7dd79615829964f3f97ba5b0c6d4
Masud Pervez <rocklucifer113@gmail.com>
2022-04-22 12:03:52 +0600

    03

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,17 @@
+(function ($) {
+  "use strict";
 
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight();
 
-(function($) {
-
-	"use strict";
-
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
   });
-
 })(jQuery);
 
-
+// image
