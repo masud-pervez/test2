293db27899381d8a4c46fa0bc94fe80f9ea284e5
Masud Pervez <rocklucifer113@gmail.com>
2022-04-28 11:43:01 +0600

    52

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,16 @@
 })(jQuery);
 
 // image
+
+let backtotop = select('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
+    }
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
